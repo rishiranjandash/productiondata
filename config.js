34d7c0133c7f28cebb0b95e99641738692093b6a
rishiranjandash <rishi.dash@awign.com>
2026-09-18 04:47:37 +0530

    Initialize config.js with OAuth settings
    Added configuration for Google OAuth and Apps Script URL.

---
+++ config.js
@@ -0,0 +1,7 @@
+// Fill these in after you deploy the Apps Script backend and create
+// (or reuse) a Google OAuth Client ID. See ../README.md.
+
+const CONFIG = {
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbyH_wuhstbPZfxqcyQqOsVkkbkThkdEiaQcAv7rmkVHixvKxUrQQWn1UVR0hygch5sN/exec', // ends in /exec
+  GOOGLE_CLIENT_ID: '467836469491-fe7gpq5l2altnjibkjn6bbdrddr5nuhn.apps.googleusercontent.com'
+};
