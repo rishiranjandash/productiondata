c69d7773a4cbe03fc492190bce979a4ee17bc488
rishiranjandash <rishi.dash@awign.com>
2026-09-18 05:42:24 +0530

    Change APPS_SCRIPT_URL to new Google Script URL
    Updated the APPS_SCRIPT_URL to a new endpoint.

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 // (or reuse) a Google OAuth Client ID. See ../README.md.
 
 const CONFIG = {
-  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbyH_wuhstbPZfxqcyQqOsVkkbkThkdEiaQcAv7rmkVHixvKxUrQQWn1UVR0hygch5sN/exec', // ends in /exec
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbwV92sBuGbzGBCWVd-t4_SxvZmXe2gx3J62KKVWahNmJGIzJD66ed7M96-KUeMcgQwy/exec', // ends in /exec
   GOOGLE_CLIENT_ID: '467836469491-fe7gpq5l2altnjibkjn6bbdrddr5nuhn.apps.googleusercontent.com'
 };
